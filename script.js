de3d9e0e1d818244b96c50d3f45b930ee46b45c9
iPiotr <ip.starzec@gmail.com>
2020-01-21 23:05:16 +0100

    UP

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ const table = [
     ['', '', ''],
     ['', '', '']
 ];
+
 const combinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
 
+
 const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => box.addEventListener('click', pick));
-
+ 
 $('.reset').click(function () {
     location.reload(); 
 });
